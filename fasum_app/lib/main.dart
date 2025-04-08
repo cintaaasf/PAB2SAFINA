18a813c93e1a7a3708d4680b56bb156a836011bd
cintaaasf <riadisafina@gmail.com>
2025-04-08 13:10:03 +0700

    update fasum app

--- fasum_app/lib/main.dart
+++ fasum_app/lib/main.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fasum_app/firebase_options.dart';
+import 'package:fasum_app/screens/home_screen.dart';
+import 'package:fasum_app/screens/sign_in_screen.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAppCheck.instance.activate(
+    webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
+  );
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello World"),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fasum',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          } else {
+            return const SignInScreen();
+          }
+        },
       ),
     );
   }
