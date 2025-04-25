531abba6dd63a1846926dab097c47a80baa13920
cintaaasf <riadisafina@gmail.com>
2025-04-25 11:14:17 +0700

    update fasum

--- fasum_app/lib/screens/add_post_screen.dart
+++ fasum_app/lib/screens/add_post_screen.dart
@@ -1,16 +1,16 @@
 import 'dart:convert';
+import 'dart:ffi';
 import 'dart:io';
-import 'package:flutter/material.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:shimmer/shimmer.dart';
-
-
-
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -32,34 +32,39 @@ class _AddPostScreenState extends State<AddPostScreen> {
   bool _isGenerating = false;
 
   Future<void> _compressAndEncodeImage() async {
-    if (_image == Null) return;
+    if (_image == null) return;
 
     try {
-      final CompressedImage = await FlutterImageCompress.compressWithFile(
+      final compressedImage = await FlutterImageCompress.compressWithFile(
         _image!.path,
         quality: 50,
       );
-
-      if (CompressedImage == null) return;
+      if (compressedImage == null) return;
 
       setState(() {
-        _base64Image = base64Encode(CompressedImage);
+        _base64Image = base64Encode(compressedImage);
       });
-    } catch (e) {}
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Failed to compress image: $e')));
+      }
+    }
   }
 
-  Future<void> _pickImage(ImageSource source) async {
+  Future<void>? _pickImage(ImageSource source) async {
     try {
-      final pickedFile = await _picker.pickImage(source: source);
-      if (pickedFile != null) {
+      final PickedFile = await _picker.pickImage(source: source);
+      if (PickedFile != null) {
         setState(() {
-          _image = File(pickedFile.path);
+          _image = File(PickedFile.path);
           _aiCategory = null;
           _aiDescription = null;
           _descriptionController.clear();
         });
         await _compressAndEncodeImage();
-        // await _generateDescriptionWithAi();
+        // await _generateDescriptionWithAI();
       }
     } catch (e) {
       if (mounted) {
@@ -70,8 +75,150 @@ class _AddPostScreenState extends State<AddPostScreen> {
     }
   }
 
+  Future<void> _getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever ||
+          permission == LocationPermission.denied) {
+        throw Exception('Location permissions are denied');
+      }
+      try {
+        final Position = await Geolocator.getCurrentPosition(
+          locationSettings: LocationSettings(accuracy: LocationAccuracy.high),
+        ).timeout(const Duration(seconds: 10));
+        setState(() {
+          _latitude = Position.latitude;
+          _longtitude = Position.longitude;
+        });
+      } catch (e) {
+        debugPrint('gagal mendaptkan lokasi: $e');
+        setState(() {
+          _latitude = null;
+          _longtitude = null;
+        });
+      }
+    }
+  }
+
+  Future<void> _submitPost() async {
+    if (_base64Image == null || _descriptionController.text.isEmpty) return;
+
+    setState(() => _isUploading = true);
+    final now = DateTime.now().toIso8601String();
+    final uid = FirebaseAuth.instance.currentUser?.uid;
+
+    if (uid == null) {
+      setState(() => _isUploading = false);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Pengguna tidak ditemukan')),
+      );
+      return;
+    }
+    try {
+      await _getLocation();
+
+      //ambil nama lengkap dari koleksi users
+      final userDoc =
+          await FirebaseFirestore.instance.collection('users').doc(uid).get();
+      final fullName = userDoc.data()?['fullName'] ?? 'Tanpa Nama';
+      await FirebaseFirestore.instance.collection('posts').add({
+        'image': _base64Image,
+        'description': _descriptionController.text,
+        'createAt': now,
+        'latitude': _latitude,
+        'longitude': _longtitude,
+        'fullName': fullName, // <-- ta,bahkan ini
+        'userId': uid, //Optional
+      });
+    } catch (e) {
+      debugPrint('Upload failed : $e');
+      if (!mounted) return;
+      setState(() => _isUploading = false);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Gagal mengunggah postingan')),
+      );
+    }
+  }
+
+  void _showImageSourceDialog() {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return SafeArea(
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                  leading: const Icon(Icons.camera_alt),
+                  title: const Text('Take a Picture'),
+                  onTap: () {
+                    Navigator.pop(context);
+                    _pickImage(ImageSource.camera);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.photo_library),
+                  title: const Text('Choose from gallery'),
+                  onTap: () {
+                    Navigator.pop(context);
+                    _pickImage(ImageSource.gallery);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.cancel),
+                  title: const Text('Cancel'),
+                  onTap: () => Navigator.pop(context),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Post'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.file(
+              _image!,
+              height: 250,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _descriptionController,
+              textCapitalization: TextCapitalization.sentences,
+              maxLines: 6,
+              decoration: const InputDecoration(
+                hintText: 'Add a brief description...',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Post'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
