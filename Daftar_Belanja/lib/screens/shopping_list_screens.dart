09ac5fb3c29778c4b811246aaad88f7fdcccfe56
Wildan728 <shahabwildan1225@gmail.com>
2025-05-09 11:02:34 +0700

    hapus film dan daftar belanja

--- Daftar_Belanja/lib/screens/shopping_list_screens.dart
+++
@@ -1,2 +0,0 @@
-import 'package:daftar_belanja/services/shopping_service.dart';
-import 'package:flutter/cupertino.dart';
--- film/android/app/src/main/kotlin/com/example/film/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.film
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
